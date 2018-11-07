5a8acb0107aef1c3f6c4f0ef80aaf01c6388c0a5
Andrew Davis <tuf75588@temple.edu>
2018-11-06 21:30:56 -0500

    update packages

--- src/App.js
+++ src/App.js
@@ -4,37 +4,33 @@ function createStore(reducer, initialState) {
   let state = initialState;
   const listeners = [];
 
-  const subscribe = (listener) => (
-    listeners.push(listener)
-  );
+  const subscribe = (listener) => listeners.push(listener);
 
-  const getState = () => (state);
+  const getState = () => state;
 
   const dispatch = (action) => {
     state = reducer(state, action);
-    listeners.forEach(l => l());
+    listeners.forEach((l) => l());
   };
 
   return {
     subscribe,
     getState,
-    dispatch,
+    dispatch
   };
 }
 
 function reducer(state, action) {
   if (action.type === 'ADD_MESSAGE') {
     return {
-      messages: state.messages.concat(action.message),
+      messages: state.messages.concat(action.message)
     };
   } else if (action.type === 'DELETE_MESSAGE') {
     return {
       messages: [
         ...state.messages.slice(0, action.index),
-        ...state.messages.slice(
-          action.index + 1, state.messages.length
-        ),
-      ],
+        ...state.messages.slice(action.index + 1, state.messages.length)
+      ]
     };
   } else {
     return state;
@@ -54,7 +50,7 @@ class App extends React.Component {
     const messages = store.getState().messages;
 
     return (
-      <div className='ui segment'>
+      <div className="ui segment">
         <MessageView messages={messages} />
         <MessageInput />
       </div>
@@ -64,41 +60,33 @@ class App extends React.Component {
 
 class MessageInput extends React.Component {
   state = {
-    value: '',
+    value: ''
   };
 
   onChange = (e) => {
     this.setState({
-      value: e.target.value,
-    })
+      value: e.target.value
+    });
   };
 
   handleSubmit = () => {
     store.dispatch({
       type: 'ADD_MESSAGE',
-      message: this.state.value,
+      message: this.state.value
     });
     this.setState({
-      value: '',
+      value: ''
     });
   };
 
   render() {
     return (
-      <div className='ui input'>
-        <input
-          onChange={this.onChange}
-          value={this.state.value}
-          type='text'
-        />
-        <button
-          onClick={this.handleSubmit}
-          className='ui primary button'
-          type='submit'
-        >
+      <div className="ui input">
+        <input onChange={this.onChange} value={this.state.value} type="text" />
+        <button onClick={this.handleSubmit} className="ui primary button" type="submit">
           Submit
         </button>
-       </div>
+      </div>
     );
   }
 }
@@ -107,25 +95,19 @@ class MessageView extends React.Component {
   handleClick = (index) => {
     store.dispatch({
       type: 'DELETE_MESSAGE',
-      index: index,
+      index: index
     });
   };
 
   render() {
     const messages = this.props.messages.map((message, index) => (
-      <div
-        className='comment'
-        key={index}
-        onClick={() => this.handleClick(index)}
-      >
+      <div className="comment" key={index} onClick={() => this.handleClick(index)}>
         {message}
       </div>
     ));
     return (
-      <div className='ui center aligned basic segment'>
-        <div className='ui comments'>
-          {messages}
-        </div>
+      <div className="ui center aligned basic segment">
+        <div className="ui comments">{messages}</div>
       </div>
     );
   }
