10e1ad2ae1b1588c4a44275436e80cb32d1ca06d
Andrew Davis <tuf75588@temple.edu>
2018-11-14 21:37:59 -0500

    reducer refactor

--- src/reducers/threadsReducers.js
+++ src/reducers/threadsReducers.js
@@ -18,7 +18,7 @@ export function threadsReducer(state, action) {
     const oldThread = state[threadIndex];
     const newThread = {
       ...oldThread,
-      messages: oldThread.messages.concat(newMessage)
+      messages: messageReducer(oldThread.messages, action)
     };
 
     return [...state.slice(0, threadIndex), newThread, ...state.slice(threadIndex + 1, state.length)];
@@ -28,11 +28,33 @@ export function threadsReducer(state, action) {
 
     const newThread = {
       ...oldThread,
-      messages: oldThread.messages.filter((m) => m.id !== action.id)
+      messages: messageReducer(oldThread.messages, action)
     };
 
     return [...state.slice(0, threadIndex), newThread, ...state.slice(threadIndex + 1, state.length)];
   } else {
     return state;
+  }
+}
+export function messageReducer(state, action) {
+  if (action.type === 'ADD_MESSAGE') {
+    const newMessage = {
+      text: action.text,
+      timestamp: Date.now(),
+      id: uuid.v4()
+    };
+    return state.concat(newMessage);
+  } else {
+    return state;
+  }
+}
+function findThreadIndex(threads, action) {
+  switch (action.type) {
+    case 'ADD_MESSAGE':
+      return threads.findIndex((m) => m.id === action.threadId);
+    case 'DELETE_MESSAGE':
+      return threads.findIndex((t) => t.messages.find((m) => m.id === action.id));
+    default:
+      return threads;
   }
 }
